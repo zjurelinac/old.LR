0512c9d8f8d7e755aec5b7442edddba5fb72ac35
Zvonimir Jurelinac <zjurelinac@gmail.com>
2015-06-21 22:07:54 +0200

    Started creating a database init file - containing code for creating test data to insert into the database

---
+++ database_init.py
@@ -0,0 +1,14 @@
+from models import *
+
+u1 = User.register( 'Mirko Mirković', 'mirko@mail.com', 'lozinka1' )
+u2 = User.register( 'Slavko Slavković', 'slavko@mail.com', 'zaporka1' )
+u3 = User.register( 'Francisco Goya', 'fgoya@mail.com', 'ThirdOfMay1808' )
+u4 = User.register( 'Pablo Picasso', 'ppicasso@mail.com', 'Guernica' )
+u5 = User.register( 'Vincent Van Gogh', 'vvgogh@mail.com', 'StarryNight' )
+u6 = User.register( 'Claude Monet', 'cmonet@mail.com', '' )
+u7 = User.register( 'Salvador Dali', 'sdali@mail.com', '' )
+u8 = User.register( '', '', '' )
+u9 = User.register( '', '', '' )
+u10 = User.register( '', '', '' )
+
+
